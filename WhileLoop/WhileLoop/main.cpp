6b876860d9525d9617c14be2f44b7797411761f1
UnicornDad8 <unicorndad8@gmail.com>
2023-12-29 13:15:03 -0300

    Added example of a while loop in C++

---
+++ WhileLoop/WhileLoop/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  WhileLoop
+//
+//  Created by Ceci Benitez on 12/29/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 1;
+    while(i <= 10) {
+        cout << i << endl;
+        i++;
+    }
+   
+    return 0;
+}
