16c23ede38e5368b3e5f4b6bf509f4c932dec78b
UnicornDad8 <unicorndad8@gmail.com>
2023-12-29 10:51:38 -0300

    Example of for loop in C++

---
+++ TheForLoop/TheForLoop/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  TheForLoop
+//
+//  Created by Ceci Benitez on 12/29/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for(int i = 0; i < 5; i++)
+        cout << i << endl;
+    
+    return 0;
+}
