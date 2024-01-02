47dd30d8f26883d1c2c75f725ac9c783d1a0d149
UnicornDad8 <unicorndad8@gmail.com>
2024-01-02 12:52:14 -0300

    Passing arguments by value

---
+++ PassingArguments/PassingArguments/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  PassingArguments
+//
+//  Created by Ceci Benitez on 1/2/24.
+//
+
+#include <iostream>
+
+using namespace std;
+
+double increasePrice(double price) {
+    price *= 1.2;
+    return price;
+}
+
+int main() {
+    double price = 100;
+    price = increasePrice(price);
+    cout << price << endl;
+    
+    return 0;
+}
