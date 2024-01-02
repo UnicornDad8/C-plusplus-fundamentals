23ce63f39f3706f10c51a1a847b9c446155c02ba
UnicornDad8 <unicorndad8@gmail.com>
2024-01-02 12:55:08 -0300

    Passing arguments by reference

--- PassingArguments/PassingArguments/main.cpp
+++ PassingArguments/PassingArguments/main.cpp
@@ -9,14 +9,20 @@
 
 using namespace std;
 
+/*
 double increasePrice(double price) {
     price *= 1.2;
     return price;
 }
+*/
+
+void increasePrice(double& price) {
+    price *= 1.2;
+}
 
 int main() {
     double price = 100;
-    price = increasePrice(price);
+    increasePrice(price);
     cout << price << endl;
     
     return 0;
